e77714dcfe84013d280192c09e5317dd9a7c063c
D1perblue <72693895+Diperblue@users.noreply.github.com>
2022-04-08 00:25:18 +0000

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,13 +1,5 @@
 import argparse
 
-dic = {'<':'%253C', '>':'%253E', '/':'%252F'}
-
-parser = argparse.ArgumentParser(description='Double url encoding for xss payload.')
-parser.add_argument('-p', '--payload', action='store', dest='payload',
-					required=True, help="The payload you want to encode.")
-arguments = parser.parse_args()
-payload = arguments.payload
-
 a = """ 
 	
 ██████╗░░█████╗░██╗░░░██╗██████╗░██╗░░░░░███████╗  ██╗░░░██╗██████╗░██╗░░░░░
@@ -27,9 +19,26 @@ a = """
 
 print(a)
 
-payload = payload.replace('<', dic['<'])
-payload = payload.replace('>', dic['>'])
-payload = payload.replace('/', dic['/'])
+dic_o = {'<': '%3C', '>': '%3E', '/': '%2F'}
+dic_d = {'<':'%253C', '>':'%253E', '/':'%252F'}
+
+parser = argparse.ArgumentParser(description='Double url encoding for xss payload.')
+parser.add_argument('-p', '--payload', action='store', dest='payload',
+					required=True, help="The payload you want to encode.")
+parser.add_argument('-t', '--type', action='store', dest='type',
+					required=False, default=1, help="Type of encode")
+arguments = parser.parse_args()
+payload = arguments.payload
+type_d = arguments.type
+
+if type_d == 'o':
+	payload = payload.replace('<', dic_o['<'])
+	payload = payload.replace('>', dic_o['>'])
+	payload = payload.replace('/', dic_o['/'])
+else:
+	payload = payload.replace('<', dic_d['<'])
+	payload = payload.replace('>', dic_d['>'])
+	payload = payload.replace('/', dic_d['/'])
 
 print('Payload encoded!!:\n')
 print(payload)
